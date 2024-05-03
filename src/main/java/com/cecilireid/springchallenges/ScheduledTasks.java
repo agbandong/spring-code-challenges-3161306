87386a474a2963775ddd24bbbdb74ff7110afacd
agbandong <69942400+agbandong@users.noreply.github.com>
2024-05-03 16:27:48 +0800

    Update: Added scheduled tasks to see number of jobs.

--- src/main/java/com/cecilireid/springchallenges/ScheduledTasks.java
+++ src/main/java/com/cecilireid/springchallenges/ScheduledTasks.java
@@ -2,13 +2,23 @@ package com.cecilireid.springchallenges;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ScheduledTasks {
-    private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
+    private final CateringJobRepository cateringJobRepository;
+    public ScheduledTasks(CateringJobRepository cateringJobRepository){
+        this.cateringJobRepository = cateringJobRepository;
+    }
 
+    private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
+    @Scheduled(fixedRate = 10000)
     public void reportOrderStats() {
-        
+        List<CateringJob> jobs= cateringJobRepository.findAll();
+        logger.info("We have {} jobs", jobs.size());
+        //logger.info("Jobs:{}{}", System.lineSeparator(), jobs);
     }
 }
