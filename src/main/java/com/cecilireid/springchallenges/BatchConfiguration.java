e0f19549255c367bbda15b2786745bf8b2d1d10d
agbandong <69942400+agbandong@users.noreply.github.com>
2024-05-03 14:48:23 +0800

    Update: Updated spring and java version of project

--- src/main/java/com/cecilireid/springchallenges/BatchConfiguration.java
+++ src/main/java/com/cecilireid/springchallenges/BatchConfiguration.java
@@ -3,8 +3,8 @@ package com.cecilireid.springchallenges;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
@@ -16,15 +16,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
+
+    //Note: use constructor injector
+    //https://www.baeldung.com/java-spring-field-injection-cons
+    //https://www.baeldung.com/spring-injection-lombok
     @Autowired
     public CateringJobRepository repository;
 
+    /*
+    Depreciated
+    See
+    https://stackoverflow.com/questions/75508267/how-to-solve-deprecation-warning-of-jobbuilderfactory-and-stepbuilderfactory
     @Autowired
     public JobBuilderFactory jobBuilderFactory;
 
     @Autowired
     public StepBuilderFactory stepBuilderFactory;
-
+    */
     @Bean
     @StepScope
     public FlatFileItemReader<CateringJob> reader() {
