ed70d9c9cad91ca1859e5e42e4909a9db722794c
agbandong <69942400+agbandong@users.noreply.github.com>
2024-05-15 09:15:51 +0800

    Update: Added BatchConfiguration: Not working yet.

--- src/main/java/com/cecilireid/springchallenges/BatchConfiguration.java
+++ src/main/java/com/cecilireid/springchallenges/BatchConfiguration.java
@@ -3,16 +3,20 @@ package com.cecilireid.springchallenges;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.transaction.PlatformTransactionManager;
 
+//todo: Note not working check how make work
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
@@ -20,8 +24,18 @@ public class BatchConfiguration {
     //Note: use constructor injector
     //https://www.baeldung.com/java-spring-field-injection-cons
     //https://www.baeldung.com/spring-injection-lombok
-    @Autowired
-    public CateringJobRepository repository;
+    /*@Autowired
+    public CateringJobRepository repository;*/
+
+    private final CateringJobRepository cateringJobRepository;
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+
+    public BatchConfiguration(CateringJobRepository cateringJobRepository, JobRepository jobRepository, PlatformTransactionManager transactionManager){
+        this.cateringJobRepository = cateringJobRepository;
+        this.jobRepository = jobRepository;
+        this.transactionManager = transactionManager;
+    }
 
     /*
     Depreciated
@@ -33,27 +47,45 @@ public class BatchConfiguration {
     @Autowired
     public StepBuilderFactory stepBuilderFactory;
     */
+
     @Bean
     @StepScope
     public FlatFileItemReader<CateringJob> reader() {
         FlatFileItemReader<CateringJob> reader = new FlatFileItemReader<>();
+        reader.setResource(new ClassPathResource("upload.csv"));
+        reader.setLinesToSkip(1);
+        DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
+
+        //Find way to automate set tokenizer
+        tokenizer.setNames("id", "customerName", "phoneNumber", "email", "menu", "noOfGuests", "status");
         DefaultLineMapper<CateringJob> lineMapper = new DefaultLineMapper<>();
+        //DelimitedLineTokenizer defaults to comma as its delimiter
+        lineMapper.setLineTokenizer(tokenizer);
+        lineMapper.setFieldSetMapper(new CateringJobMapper());
+        reader.setLineMapper(lineMapper);
         return reader;
     }
 
     @Bean
     public RepositoryItemWriter<CateringJob> writer() {
-//        RepositoryItemWriter<CateringJob> writer = new RepositoryItemWriter<>();
-        return null;
-    }
-
-    @Bean
-    public Job uploadCateringJob() {
-        return null;
+        RepositoryItemWriter<CateringJob> writer = new RepositoryItemWriter<>();
+        writer.setRepository(cateringJobRepository);
+        return writer;
     }
 
     @Bean
     public Step step() {
-        return null;
+        return new StepBuilder("step", jobRepository)
+                .<CateringJob, CateringJob>chunk(10, transactionManager)
+                .reader(reader())
+                .writer(writer())
+                .build();
+    }
+
+    @Bean
+    public Job uploadCateringJob() {
+        return new JobBuilder("uploadCateringJob", jobRepository)
+                .start(step())
+                .build();
     }
 }
--- src/main/java/com/cecilireid/springchallenges/WebExceptions.java
+++ src/main/java/com/cecilireid/springchallenges/WebExceptions.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.HttpClientErrorException;
 
+//todo: This broke I don't know why or how.
 public class WebExceptions {
     @ExceptionHandler(HttpClientErrorException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
@@ -15,6 +16,11 @@ public class WebExceptions {
     @ExceptionHandler(HttpClientErrorException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     String handleHttpBadRequestErrorException(HttpClientErrorException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    String handleHttpErrorException(HttpClientErrorException e) {
         return e.getMessage();
     }
 }
